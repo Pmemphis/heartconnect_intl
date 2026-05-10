3131a7d3f3dd4c00eab6bf9bc4260d2ff3c49e82
Pmemphis <pinidymwendia8@gmail.com>
2026-05-11 02:04:55 +0300

    Update WhatsApp number and personalize message

--- members/views.py
+++ members/views.py
@@ -18,7 +18,7 @@ def register(request):
 
 def payment(request, user_id):
     user = Applicant.objects.get(id=user_id)
-    phone = "254XXXXXXXXX" # Put your M-Pesa/WhatsApp number here
+    phone = "254741189878" # Put your M-Pesa/WhatsApp number here
     text = f"Hello, I am {user.full_name}. I just registered on HeartConnect and want to pay the $15 fee."
     whatsapp_url = f"https://wa.me/{phone}?text={urllib.parse.quote(text)}"
     
