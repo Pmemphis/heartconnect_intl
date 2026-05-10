6824c2442ab4c03fb014c2b9b0d16453441b59dc
Pmemphis <pinidymwendia8@gmail.com>
2026-05-11 01:24:32 +0300

    Fix database engine configuration

--- core/settings.py
+++ core/settings.py
@@ -80,9 +80,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(
+        # This looks for the environment variable 'DATABASE_URL' automatically
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600
+    )
 }
-
 
 
 # Password validation
